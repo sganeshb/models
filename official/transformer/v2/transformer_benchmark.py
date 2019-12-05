b1133133e51e31ff4608d02b45baf4e016f8a3d3
sganeshb <53017143+sganeshb@users.noreply.github.com>
2019-12-04 21:41:25 -0800

    Update transformer_benchmark.py

--- official/transformer/v2/transformer_benchmark.py
+++ official/transformer/v2/transformer_benchmark.py
@@ -218,8 +218,8 @@ class TransformerBaseKerasAccuracy(TransformerBenchmark):
     FLAGS['bleu_ref'].value = self.bleu_ref
     FLAGS.param_set = 'base'
     FLAGS.batch_size = 4096*8
-    FLAGS.train_steps = 100000
-    FLAGS.steps_between_evals = 20000
+    FLAGS.train_steps = 20000
+    FLAGS.steps_between_evals = 10000
     FLAGS.model_dir = self._get_model_dir('benchmark_8_gpu')
     self._run_and_report_benchmark(total_batch_size=FLAGS.batch_size,
                                    log_steps=FLAGS.log_steps,
